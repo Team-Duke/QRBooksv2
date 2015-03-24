df356ab2d714dff0ee0559ad1b6538322ef2c92c
Calvin-Scott <calvinjohnscott@gmail.com>
2015-03-24 09:46:13 +0000

    changes made for accessing second activity

--- app/src/main/java/teamduke/qrbooksv2/MainActivity.java
+++ app/src/main/java/teamduke/qrbooksv2/MainActivity.java
@@ -1,9 +1,11 @@
 package teamduke.qrbooksv2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -15,6 +17,12 @@ public class MainActivity extends Activity {
     }
 
 
+    public void Book_Activity(View view) {
+        Intent getBookActivity = new Intent(this,BookActivity.class);
 
+        final int result = 1;
+        getBookActivity.putExtra("callingActivity", "MainActivity");
+        startActivityForResult(getBookActivity, result);
 
+    }
 }
